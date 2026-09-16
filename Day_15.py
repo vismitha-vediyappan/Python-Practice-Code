471d2a79317e0e71fa409af367126c0e86c5407b
vismitha-vediyappan <vismitha389@gmail.com>
2026-09-16 16:53:39 +0530

    Day 15

---
+++ Day_15.py
@@ -0,0 +1,40 @@
+#FUNCTIONS
+#1.Hello Python
+def hello ():
+    print("Hello Python")
+hello()
+
+#2.Student Information
+def student ():
+    print("Name:Vismitha")
+    print("College:ABC Engineering College")
+    print("Branch:ECE")
+student()
+
+#3.Add Two Numbers
+def add (a,b):
+    c = a + b
+    print(c)
+add(10,20)
+
+#4.Subtract Two Numbers
+def sub (a,b):
+    c = a - b
+    print(c)
+sub(20,8)
+
+#5.Multiply Two Numbers
+def multiply (a,b):
+    c = a * b
+    print(c)
+multiply(5,4)
+
+#6.Even or Odd
+def even_or_odd(n):
+    if n %2 == 0 :
+        print("Even")
+    else:
+        print("Odd")
+even_or_odd(19)
+
+
