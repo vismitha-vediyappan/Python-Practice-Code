b301be751f102f05acb84e6efff8bf7f3dcefdff
vismitha-vediyappan <vismitha389@gmail.com>
2026-09-24 04:53:22 +0530

    Dictionaries

---
+++ Day_28.py
@@ -0,0 +1,20 @@
+#DICTIONARIES
+#51.Create and print a dictionary.
+my_dict = { 1 : "one", 2 : "two" , 3 : "three" , 4 : "four"}
+print(my_dict)
+
+print(my_dict[3])#52.Access a value using its key
+
+#53.Add a new key-value pair
+my_dict [ 5 ]  = "five"
+print(my_dict)
+
+#54.Update an existing value
+my_dict.update({3 : "THREE"})
+print(my_dict)
+
+print(my_dict.keys())#55.print all keys
+print(my_dict.values())#55.print all values
+print(my_dict.items())#55.print key-value pairs
+
+
